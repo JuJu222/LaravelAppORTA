f9638d4ce5f19aeca230b9cb034bbf525a9f883e
JuJu222 <japjustin@yahoo.com>
2023-01-20 23:21:40 +0700

    temp

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,17 +1,16 @@
 import React, { useState } from "react";
 import {Link} from "@inertiajs/inertia-react";
-function IndexPage() {
+import ApplicationLogo from "@/Components/ApplicationLogo";
+function Welcome() {
     const [menu, setMenu] = useState(false);
     return (
         <>
             <div className="relative w-full h-full pb-10">
-                <div className="hidden md:block">
-                    <img className="absolute bg-cover bg-center w-full h-full inset-0" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/Hero%2008.png" alt />
-                </div>
                 <nav className="lg:hidden relative z-50">
                     <div className="flex py-2 justify-between items-center px-4">
                         <div>
-                            <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/large_typography_with_gradient_and_glass_effect_Svg1.svg" alt="logo" />
+                            <ApplicationLogo white={false} className="w-12 h-12" />
+
                         </div>
                         <div className="visible flex items-center">
                             <button id="open" onClick={() => setMenu(!menu)} className={` ${menu ? 'hidden' : ''} focus:outline-none focus:ring-2 focus:ring-black`}>
@@ -72,17 +71,16 @@ function IndexPage() {
                                     <a href="javascript:void(0)" className="focus:outline-none focus:ring-2 focus:ring-white">Resources</a>
                                 </li>
                             </ul>
-                            <Link href={route('login')} className="focus:outline-none lg:text-lg lg:font-bold focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 hidden md:block bg-transparent transition duration-150 ease-in-out hover:bg-gray-200 rounded border border-indigo-700 text-indigo-700 px-4 sm:px-8 py-1 sm:py-3 text-sm">Sign In</Link>
-                            <button className="px-6 py-3 bg-indigo-400 hover:bg-indigo-500 text-white text-base font-medium rounded-lg flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700">
+                            <Link href={route('login')} className="px-6 py-3 bg-indigo-400 hover:bg-indigo-500 text-white text-base font-medium rounded-lg flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700">
                                 Sign In
                                 <img className="ml-4" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/large_typography_with_gradient_and_glass_effect_Svg5.svg" alt="arrow" />
-                            </button>
+                            </Link>
                         </div>
                     </div>
                 </nav>
-                <div className="relative px-4 xl:px-0 container mx-auto md:flex items-center gap-8">
+                <div className="relative px-4 xl:px-0 container mx-auto md:flex items-center justify-center gap-8">
                     <div className="text-color w-full md:w-1/3 pt-16 lg:pt-32 xl:pt-12">
-                        <h1 className="text-4xl md:text-4xl lg:text-6xl w-11/12 lg:w-11/12 xl:w-full xl:text-6xl text-gray-900 font-extrabold f-f-l">Build, review and ship with ease</h1>
+                        <h1 className="text-4xl md:text-4xl lg:text-6xl w-11/12 lg:w-11/12 xl:w-full xl:text-6xl text-gray-900 font-extrabold f-f-l">Yuk, mulailah segala sesuatu dengan cinta</h1>
                         <div className="f-f-r text-base lg:text-base pb-20 sm:pb-0 pt-10 xl:pt-6">
                             <h2>The next big thing starts here. Start building with Prodify and be the first to market with a product that is ready to take on the competition and delight your customers</h2>
                         </div>
@@ -91,7 +89,7 @@ function IndexPage() {
                             <button className="hidden md:block hover:opacity-90 text-base w-full xl:text-base xl:w-4/12 lg:ml-2 xl:ml-2 mt-4 xl:mt-8 f-f-r py-4  bg-indigo-200 text-indigo-600 font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 rounded-lg">Try it out</button>
                         </div>
                     </div>
-                    <img className="w-full mt-8 md:mt-0 object-fill md:w-2/3 md:-ml-4 lg:-ml-4 xl:ml-0" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/Graphic.png" alt="sample page" role="img" />
+                    <img className="w-full mt-8 md:mt-0 object-fill md:w-1/3 md:-ml-4 lg:-ml-4 xl:ml-0" src="/img/landing.png" alt="sample page" role="img" />
                     <button className="md:hidden hover:opacity-90 text-base w-full xl:text-base xl:w-6/12 mt-4 xl:mt-8 f-f-r py-4  bg-indigo-700 text-white font-bold focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 rounded-lg">Start building now</button>
                     <button className="md:hidden hover:opacity-90 text-base w-full xl:text-base xl:w-4/12 xl:ml-2 mt-4 xl:mt-8 f-f-r py-4  bg-indigo-200 text-indigo-600 font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 rounded-lg">Try it out</button>
                 </div>
@@ -122,4 +120,4 @@ function IndexPage() {
     );
 }
 
-export default IndexPage;
+export default Welcome;
