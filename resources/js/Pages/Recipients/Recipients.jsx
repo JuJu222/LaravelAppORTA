d86924f2bedafaf6876a9d6905c1bc9032542454
JuJu222 <japjustin@yahoo.com>
2023-01-22 13:03:34 +0700

    temp

--- resources/js/Pages/Recipients/Recipients.jsx
+++ resources/js/Pages/Recipients/Recipients.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Inertia} from "@inertiajs/inertia";
 import Authenticated from "@/Layouts/AuthenticatedLayout";
 import {Link} from "@inertiajs/inertia-react";
 import RecipientCardAdmin from "@/Components/RecipientCardAdmin";
 
 export default function Recipients(props) {
+    const [filteredProducts, setFilteredProducts] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
+
+    // your search event handler
+    function search (searchTerm) {
+        // update search value
+        setSearchValue(searchTerm);
+
+        const filtered = getValues("products").filter(
+            product =>
+                product.description.toLowerCase().indexOf(searchTerm) > -1,
+        );
+
+        // set filtered products in state
+        setFilteredProducts(filtered);
+    }
 
     function handleDelete(id) {
         Inertia.delete(route("recipients.destroy", id));
@@ -22,7 +38,7 @@ export default function Recipients(props) {
                         <input type="text" id="username" name="username"
                                className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red focus:border-red block w-full p-2.5 placeholder-gray-400"
                                placeholder="Cari wali anak" />
-                        <Link href={route("disabilities.create")}>
+                        <Link href={route("recipients.create")}>
                             <button className="inline-flex ml-4 sm:mt-0 items-start justify-start px-5 py-2.5 bg-red hover:bg-red_hover transition focus:outline-none rounded">
                                 <p className="text-xl font-medium leading-none text-white">+</p>
                             </button>
